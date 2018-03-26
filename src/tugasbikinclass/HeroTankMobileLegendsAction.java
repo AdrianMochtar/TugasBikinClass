f37da7d7bf194e9fc06f237af9545a3bc628a062
AdrianMochtar <mochtaradrian.am@gmail.com>
2018-03-26 21:44:20 +0800

    Membuat Instance Object

--- src/tugasbikinclass/HeroTankMobileLegendsAction.java
+++ src/tugasbikinclass/HeroTankMobileLegendsAction.java
@@ -6,6 +6,9 @@ package tugasbikinclass;
  */
 public class HeroTankMobileLegendsAction {
     public static void main(String[] args) {
+        HeroTankMobileLegends Hylos = new HeroTankMobileLegends();
+        HeroTankMobileLegends Jhonson = new HeroTankMobileLegends();
+        HeroTankMobileLegends Akai = new HeroTankMobileLegends();
         
     }
     
