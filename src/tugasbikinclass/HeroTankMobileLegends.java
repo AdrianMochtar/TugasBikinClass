e68eeee2582d62f05cc795f818c192bb7ee9d61e
AdrianMochtar <mochtaradrian.am@gmail.com>
2018-03-26 21:36:59 +0800

    Membuat Atribut dan Behavior

--- src/tugasbikinclass/HeroTankMobileLegends.java
+++ src/tugasbikinclass/HeroTankMobileLegends.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tugasbikinclass;
 
 /**
@@ -10,5 +5,19 @@ package tugasbikinclass;
  * @author Asus Saya
  */
 public class HeroTankMobileLegends {
+    String nama;
+    Integer atk;
+    Integer hp;
+    Integer mana;
+    Integer deff;
+    
+    
+    void SupportTeam(){
+        System.out.println("Nama        : "+nama);
+        System.out.println("Attack      : "+atk);
+        System.out.println("Health Poin : "+hp);
+        System.out.println("Mana        : "+mana);
+        System.out.println("Deffend     : "+deff); 
+} 
     
 }
