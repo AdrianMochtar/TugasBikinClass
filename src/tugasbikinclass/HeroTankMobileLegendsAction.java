015d2a1490e0c5a88b3446d81bc6ff7d96d80cb4
AdrianMochtar <mochtaradrian.am@gmail.com>
2018-03-26 21:57:36 +0800

    Mengisi Atribut

--- src/tugasbikinclass/HeroTankMobileLegendsAction.java
+++ src/tugasbikinclass/HeroTankMobileLegendsAction.java
@@ -10,6 +10,24 @@ public class HeroTankMobileLegendsAction {
         HeroTankMobileLegends Jhonson = new HeroTankMobileLegends();
         HeroTankMobileLegends Akai = new HeroTankMobileLegends();
         
+        Hylos.nama="Hylos";
+        Hylos.atk=105;
+        Hylos.hp=3309;
+        Hylos.mana=430;
+        Hylos.deff=14;
+        
+        Jhonson.nama="Jhonson";
+        Jhonson.atk=112;
+        Jhonson.hp=2809;
+        Jhonson.mana=0;
+        Jhonson.deff=19;
+        
+        Akai.nama="Akai";
+        Akai.atk=115;
+        Akai.hp=2769;
+        Akai.mana=422;
+        Akai.deff=17;
+        
     }
     
 }
